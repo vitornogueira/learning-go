5daaee9832917f733188b1de08e71b6f6e3da089
Vitor Nogueira <vnweb.contato@gmail.com>
2019-12-30 17:04:35 -0300

    Add native repeat string

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,12 +1,8 @@
 package iteration
 
+import "strings"
+
 // Repeat a new string consisting of count copies of the string s
 func Repeat(character string, repeatCount int) string {
-	var repeated string
-
-	for i := 0; i < repeatCount; i++ {
-		repeated += character
-	}
-
-	return repeated
+	return strings.Repeat(character, repeatCount)
 }

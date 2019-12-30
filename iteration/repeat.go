f94f77b25bde531f40a50264b96be802afb81724
Vitor Nogueira <vnweb.contato@gmail.com>
2019-12-30 16:58:14 -0300

    Add repeatCount parameter and example for iteration

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,7 @@
 package iteration
 
-const repeatCount = 5
-
-// Repeat returns character repeated 5 times
-func Repeat(character string) string {
+// Repeat a new string consisting of count copies of the string s
+func Repeat(character string, repeatCount int) string {
 	var repeated string
 
 	for i := 0; i < repeatCount; i++ {
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,18 +1,34 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	assertCorrectMessage := func(t *testing.T, got, expected string) {
+		t.Helper()
+		if got != expected {
+			t.Errorf("expected %q but got %q", expected, got)
+		}
 	}
+
+	t.Run("repeat string with times suplied", func(t *testing.T) {
+		got := Repeat("a", 10)
+		expected := "aaaaaaaaaa"
+		assertCorrectMessage(t, got, expected)
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 10)
 	}
+}
+
+func ExampleRepeat() {
+	characters := Repeat("a", 6)
+	fmt.Println(characters)
+	// Output: aaaaaa
 }

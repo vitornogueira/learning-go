1e4f25a1acec7ddbcc293598a084edec94360c29
Vitor Nogueira <vnweb.contato@gmail.com>
2019-12-30 16:03:23 -0300

    Initial commit

---
+++ hello/hello_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+        t.Helper()
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    }
+
+	t.Run("say hello for peoples", func(t *testing.T) {
+		got := Hello("Vitor")
+		want := "Hello, Vitor"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when a empty string is suplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
